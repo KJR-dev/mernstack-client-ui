4d72b33cdeaaec2007238328c3b8366e081774da
Jitendra Sahoo <sahooj168@gmail.com>
2025-12-18 02:36:48 +0530

    shadcn install done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <div>Welcome to pizza shop</div>
+      <Button variant="outline">Button</Button>
     </div>
   );
 }
