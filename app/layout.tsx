ff2ed2a9944d526245c6ee15c05dbfe2e65bc1bd
Jitendra Sahoo <sahooj168@gmail.com>
2025-12-18 19:00:28 +0530

    add manrope font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const manropeSans = Manrope({
+  variable: "--font-manrope-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const manropeMono = Manrope({
+  variable: "--font-manrope-mono",
   subsets: ["latin"],
 });
 
@@ -19,13 +19,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${manropeSans.variable} ${manropeMono.variable} ${manropeSans.className} antialiased`}
       >
         {children}
       </body>
